4d8854f817a2d6cb0524ffc46abe4535dd509951
Daniiilka <romanovdaniil71@gmail.com>
2022-05-23 00:03:48 +0300

    order of the final text

--- text_messages.py
+++ text_messages.py
@@ -6,7 +6,7 @@ start_message = (
 
 def result_message(data):
     result = (
-        f'• <b>Владелец:</b> {data["name"]} ({data["person"].lower()})\n\n'
+        f'• <b>Имя:</b> {data["name"]} ({data["person"].lower()})\n\n'
         f'• <b>Тип:</b> {data["type"]}\n'
         f'• <b>Район:</b> {data["district"]}\n'
         f'• <b>Комнат:</b> {data["rooms"]}\n'
@@ -14,7 +14,7 @@ def result_message(data):
         f'• <b>Кондиционер:</b> {data["conditioner"]}\n'
         f'• <b>Животные:</b> {data["pets"]}\n\n'
         f'• <b>Состояние:</b> {data["condition"]}\n\n'
-        f'• <b>Комментарий владельца:</b> {data["pros"]}\n\n'
+        f'• <b>Комментарий:</b> {data["pros"]}\n\n'
         f'• <b>Период:</b> {data["period"]}\n'
         f'• <b>Стоимость:</b> {data["price"]}\n'
         f'• <b>Контакты:</b> {data["number"]}\n'
